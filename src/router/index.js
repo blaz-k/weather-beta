74086129dea4c2188521dc881283693e58b892a1
blaz <blazyy@gmail.com>
2021-10-15 11:04:34 +0200

    change in router

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,13 @@
 import {createRouter, createWebHashHistory} from 'vue-router'
 import Cities from '../pages/Cities.vue'
 import Home from '../pages/Home.vue'
+import CityDetails from '../pages/CityDetails.vue'
 
 
 const routes = [
   { path: '/', name:'Home' , component: Home },
   { path: '/cities', name:'Cities' , component: Cities },
-  { path: '/city-details', name: 'CityDetails',
-  component: () =>
-  import('../pages/CityDetails.vue')}
+  { path: '/city-details', name: 'CityDetails', component: CityDetails}
 ]
 
 const router = createRouter({
